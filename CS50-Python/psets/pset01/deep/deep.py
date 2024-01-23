9a943c51840b8b80c31c03d8e6c08a4d9a74007b
Redakas <andregabrielmelo@gmail.com>
2024-01-23 16:55:50 -0300

    Deep pset01

--- CS50-Python/psets/pset01/deep/deep.py
+++ CS50-Python/psets/pset01/deep/deep.py
@@ -1,5 +1,11 @@
 def main():
+    # Get user input as str and clean whitespaces, turn into lower case 
+    user_input = input("What is the Answer to the Great Question of Life, the Universe and Everything? ").strip().lower()
 
+    if user_input in ["42", "forty two", "forty-two"]:
+        print("Yes")
+    else:
+        print("No")
 
 
 main()
